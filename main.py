01e5ec334eb4a752b53b87bd2abe40e3f5cd5c51
Vladimir <vi142696@gmail.com>
2021-12-30 03:43:11 +0300

    feat: add prediction functionality

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import torch
-from model import create_model
+import model
+from Langs import langs
+
+import time
 
 
-encoder, decoder = create_model()
+MAX_LENGTH = 10
+
+encoder, decoder = model.create_model(MAX_LENGTH)
+
+eng_word2index, fra_word2index, eng_index2word, fra_index2word = langs.read_langs()
+snt = input()
+
+start = time.time()
+print(model.predict(encoder, decoder, snt, eng_word2index, fra_index2word, MAX_LENGTH))
+print(time.time() - start)
--- model.py
+++ model.py
@@ -1,7 +1,19 @@
 import torch
 
+from Langs import langs
+
+
 ENCODER_WEIGHTS_PATH = "modelWeights/encoder_w.pth"
 DECODER_WEIGHTS_PATH = "modelWeights/decoder_w.pth"
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+INPUT_SIZE = 2803
+OUTPUT_SIZE = 4345
+HIDDEN_SIZE = 256
+
+SOS_token = 0
+EOS_token = 1
 
 
 class Encoder(torch.nn.Module):
@@ -38,7 +50,7 @@ class DecoderAttention(torch.nn.Module):
         self.attention_combine = torch.nn.Linear(self.hidden_size * 2, self.hidden_size)
         self.dropout = torch.nn.Dropout(self.dropout_p)
 
-        self.rnn = torch.nn.GRU(self.hidden_size, self.hidden_size)
+        self.gru = torch.nn.GRU(self.hidden_size, self.hidden_size)
         self.out = torch.nn.Linear(self.hidden_size, self.output_size)
         self.softmax = torch.nn.Softmax(dim=1)
         self.log_softmax = torch.nn.LogSoftmax(dim=1)
@@ -51,27 +63,49 @@ class DecoderAttention(torch.nn.Module):
         attention_weights = self.softmax(
             self.attention(torch.cat((embedded[0], hidden[0]), 1))
         )
+
         attention_applied = torch.bmm(attention_weights.unsqueeze(0), encoder_outputs.unsqueeze(0))
 
         output = torch.cat((embedded[0], attention_applied[0]), 1)
         output = self.attention_combine(output).unsqueeze(0)
         output = self.relu(output)
-        output, hidden = self.rnn(output, hidden)
+        output, hidden = self.gru(output, hidden)
         output = self.log_softmax(self.out(output[0]))
         return output, hidden, attention_weights
 
 
-def create_model():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+def create_model(max_length: int):
+    encoder = Encoder(input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE, device=DEVICE)
+    decoder = DecoderAttention(output_size=OUTPUT_SIZE, hidden_size=HIDDEN_SIZE, max_length=max_length)
 
-    encoder = Encoder(input_size=2803, hidden_size=256, device=device)
-    decoder = DecoderAttention(output_size=4345, hidden_size=256)
-
-    encoder.load_state_dict(torch.load(ENCODER_WEIGHTS_PATH, map_location=torch.device(device)))
-    decoder.load_state_dict(torch.load(DECODER_WEIGHTS_PATH, map_location=torch.device(device)))
+    encoder.load_state_dict(torch.load(ENCODER_WEIGHTS_PATH, map_location=torch.device(DEVICE)))
+    decoder.load_state_dict(torch.load(DECODER_WEIGHTS_PATH, map_location=torch.device(DEVICE)))
     return encoder, decoder
 
 
-# TODO
-def predict(encoder: Encoder, decoder: DecoderAttention):
-    pass
+def predict(encoder: Encoder, decoder: DecoderAttention, sentence: str, words_dict: dict, indexes_dict: dict, max_length: int):
+    with torch.no_grad():
+        input_tensor = langs.read_sentence(sentence, words_dict)
+        input_size = input_tensor.size(0)
+        encoder_hidden = encoder.init_hidden()
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=DEVICE)
+
+        for idx in range(input_size):
+            encoder_output, encoder_hidden = encoder(input_tensor[idx].unsqueeze(0), encoder_hidden)
+            encoder_outputs[idx] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=DEVICE)
+
+        decoder_hidden = encoder_hidden
+        decoded_words = []
+
+        for idx in range(max_length):
+            decoder_output, decoder_hidden, _ = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            top_value, top_index = decoder_output.topk(1)
+            if top_index.item() == EOS_token:
+                break
+            else:
+                decoded_words.append(langs.index2word(top_index.item(), indexes_dict))
+            decoder_input = top_index.squeeze().detach()
+        return " ".join(decoded_words)
+
